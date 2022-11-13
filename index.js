566f421a5a09a7d68eb1e820d79315eaae22ad91
Jose Montero <josmael0202@hotmail.com>
2022-11-13 17:40:44 -0600

    authorization with jwt

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const connection = require('./conecction');
 const routes = require('./routes/products_routes');
+const user_routes = require('./routes/user_routes');
+const auth_jwt = require('./auth/jwt');
 
 const app = express()
-const port = 3000
+const port = process.env.port;
+//const api_route = process.env.api_route;
 
 app.use(express.json());
 app.use('/api/v1',routes);
+//auth
+//app.use(auth_jwt());
+app.use('/api/v1',user_routes);
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
