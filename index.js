7827f636684fcd6dd072795751ab3652a8934d64
Jorge Alvarez <19030136@itcelaya.edu.mx>
2022-11-13 16:56:17 -0600

    commit para actualizaciion de la api

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const connection = require('./conecction');
 const routes = require('./routes/products_routes');
+const routes2=require('./routes/categories_routes');
 
 const app = express()
-const port = 3000
+const port = 8000
 
 app.use(express.json());
 app.use('/api/v1',routes);
+app.use('/api/v1',routes2);
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Starting API STORE TEDW'))
+app.listen(port, () => console.log(`START API ON PORT:${port}!`))
