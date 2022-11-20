675425e68756c65acec5fb6bafe70c166411e777
JorgeAlvarez-itc <19030136@itcelaya.edu.mx>
2022-11-20 15:11:23 -0600

    Update user_routes.js

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -84,7 +84,6 @@ router.post('/login', async (req, res) => {
     if (!user) {
         return res.status(400).send('User not found.');
     }
-
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
         const token = jwt.sign(
             {
@@ -94,13 +93,10 @@ router.post('/login', async (req, res) => {
             secret,
             { expiresIn: '1d' }
         )
-
         res.status(200).send({ user: user.email, token: token })
     } else {
         res.status(400).send('Password is wrong!');
     }
-
-
 })
 
 
